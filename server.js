0c260d29efe883c4e1aa89c76e358fe0886ad964
nitin-person <30909597+nitin-person@users.noreply.github.com>
2020-05-26 09:44:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(3000, function () {
+    console.log('App is listening to port 3000!');
+});
